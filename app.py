9f962889ad73693ff867c26759ecc92475335f97
23030024-lab <23030024@student.bdu.edu.vn>
2026-06-29 17:24:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ with st.sidebar.expander("➕ Thêm thành viên"):
 st.subheader("👥 Danh sách thành viên")
 
 cols = st.columns(3)   # 3 hồ sơ trên 1 hàng
-
+st.write(len(row))
 for idx, row in enumerate(rows):
 
     with cols[idx % 3]:
